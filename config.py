2a56c5274f064ff9fd4ab943f9c255b7ddf3c818
ezhqing <ezhqing@gmail.com>
2018-01-12 09:35:36 +0800

    flask-redis REDIS_URL

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     REDISTOGO_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-    REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+    REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
     QUEUES = ['default']
     TASKS = ['Short task', 'Long task', 'Task raises error']
     MAX_TIME_TO_WAIT = 10
